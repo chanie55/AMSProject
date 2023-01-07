e434c1b8d5321517b08b259b32366ea6766cf446
Akem12 <pesariomekaella@gmail.com>
2023-01-08 00:06:50 +0800

    updatetemp

--- index.php
+++ index.php
@@ -19,6 +19,81 @@
             <button id = "show-login" class = "btnLogin"> LOGIN </button>
         </header>
 
+        <!--PICTURES NGA MUDAGAN -->
+        
+        <div class="body">
+            <div class="back"></div>
+            <div class="slideshow-container">
+                <div class="mySlides fade">
+                  <img src="photos/bh1.jpg" style="height: 300px; width: 500px;">
+                </div>
+
+                <div class="mySlides fade">
+                  <img src="CP2.jpg" style="height: 300px; width: 500px;">
+                </div>
+
+                <div class="mySlides fade">
+                  <img src="" style="height: 300px; width: 500px;">
+                </div>
+				
+				 <div class="mySlides fade">
+                  <img src="" style="height: 300px; width: 500px;">
+                </div>
+				
+		
+
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+                <br>
+
+                <div style="text-align:center">
+                  <span class="dot" onclick="currentSlide(1)"></span> 
+                  <span class="dot" onclick="currentSlide(2)"></span> 
+                  <span class="dot" onclick="currentSlide(3)"></span> 
+				  <span class="dot" onclick="currentSlide(4)"></span> 
+			
+				  
+                </div>
+            </div>
+            <script>
+                var slideIndex = 1;
+                showSlides(slideIndex);
+
+                function plusSlides(n) {
+                  showSlides(slideIndex += n);
+                }
+
+                function currentSlide(n) {
+                  showSlides(slideIndex = n);
+                }
+
+                function showSlides(n) {
+                  var i;
+                  var slides = document.getElementsByClassName("mySlides");
+                  var dots = document.getElementsByClassName("dot");
+                  if (n > slides.length) {slideIndex = 1}    
+                  if (n < 1) {slideIndex = slides.length}
+                  for (i = 0; i < slides.length; i++) {
+                      slides[i].style.display = "none";  
+                  }
+                  for (i = 0; i < dots.length; i++) {
+                      dots[i].className = dots[i].className.replace(" active", "");
+                  }
+                  slides[slideIndex-1].style.display = "block";  
+                  dots[slideIndex-1].className += " active";
+                }
+				
+					var time = null;
+					function setTimer() {
+						timer=setInterval(function() {	
+							plusSlides(1) ; } , 5000) ;
+						}
+						setTimer();
+				
+            </script>
+        </div>
+
         <div class = "form">
             <label class="close" title="close">&times;</label>
             
@@ -37,6 +112,7 @@
                 <label>Password</label>
                 <input type="password" name = "password">
                 <div id = "pass-error"> Password is required </div>
+                <br>
                 <a href="#">Forgot Password?</a>
                 <button type="submit" class= "loginbtn" name = "login-submit">Login</button>
             </form>
@@ -54,5 +130,7 @@
             })
         </script> 
         
+        <!--gais tempo lang nang naga dagan dagan na pic akoa lang gi push para di mawala huhuehueh -->
+
     </body>
 </html>
