d4c21c7b82e2841d49fc020483a08783b8fbdd6e
Djin6 <enhog.220444@gensan.sti.edu.ph>
2023-01-07 23:16:36 +0800

    tenantsupdat

--- tenantProfile.php
+++ tenantProfile.php
@@ -2,13 +2,7 @@
 <html>
     <head>
         <link rel = "stylesheet" href = "css\tenantProfile.css"/>
-
-        <style>
-            <?php
-                include "CSS/tenantProfile.css"
-            ?>
-        </style>
-
+        <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
     </head>
     <body>
         <header>
@@ -16,7 +10,7 @@
                 Tenant's Profile
             </h3>
         </header>
-
+    <section class="body">    
         <nav>
             <ul>
                 <li>
@@ -25,44 +19,54 @@
                         <span class="nav-item">Menu</span>
                     </a>
                 </li>
-                <li><a href="adminDashboard.php" class="menu">
+                <li><a href="adminDashboard.html" class="menu">
                     <img src="photos\home.png" alt=""> <i class="ams home"></i>
                     
                     <span class="nav-item">Home</span>
                 </a> </li>
-                <li><a href="tenantProfile.php" class="menu">
+                <li><a href="tenantProfile.html" class="menu">
                     <img src="photos\tenant.png" alt=""> <i class="ams profile"></i>
                     <span class="nav-item">Tenant's Profile</span>
                 </a> </li>
-                <li><a href="room.php" class="menu">
+                <li><a href="room.html" class="menu">
                     <img src="photos\room.png" alt=""> <i class="ams room"></i>
                     <span class="nav-item">Room Management</span>
                 </a> </li>
-                <li><a href="utility.php" class="menu">
+                <li><a href="utility.html" class="menu">
                     <img src="photos\bill.png" alt=""> <i class="ams bills"></i>
                     <span class="nav-item">Utility Bills</span>
                 </a> </li>
-                <li><a href="noticeBoard.php" class="menu">
+                <li><a href="noticeBoard.html" class="menu">
                     <img src="photos\notice.png" alt=""> <i class="ams board"></i>
                     <span class="nav-item">Notice Board</span>
                 </a> </li>
-                <li><a href="invoice.php" class="menu">
+                <li><a href="invoice.html" class="menu">
                     <img src="photos\invoice.png" alt=""> <i class="ams invoice"></i>
                     <span class="nav-item">Invoice</span>
                 </a> </li>
-                <li><a href="paymentHistory.php" class="menu">
-                    <img src="photos\history.png" alt=""> <i class="ams history"></i>
+                <li><a href="paymentHistory.html" class="menu">
+                    <img src="" alt=""> <i class="ams history"></i>
                     <span class="nav-item">Payment History</span>
                 </a> </li>
-                <li><a href="index.php" class="menu logout">
+                <li><a href="index.html" class="menu logout">
                     <img src="photos\logout.png" alt=""> <i class="ams logout"></i>
                     <span class="nav-item">Logout</span>
                 </a> </li>
             </ul>
         </nav>
-
-
-        
+        <section class ="section-body">
+            <h2>Tenants</h2>
+            <div class="container">
+                <div class="search_wrap search_wrap_1">
+                    <div class="search_box">
+                        <input type="text" class="input" placeholder="Search....">
+                        <div class="btn btn_common">
+                            <i class="fas fa-search"></i>
+                        </div>
+                    </div>
+                </div>
+        </section>
+    </section>
         <h2> </h2>
     </body>
 </html>
