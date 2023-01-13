e8b4828bc6d367605d3de05bd3b150dfe0cc162f
chanie55 <anya.sc30@gmail.com>
2023-01-12 22:42:21 -0800

    login update

--- index.php
+++ index.php
@@ -15,16 +15,23 @@
 			
 	    </div>
 		<div class="login-container">
-			<form action="login.php">
+			<form action="login.php" method = "POST">
+				
 				<img class="avatar" src="photos/avatar.svg">
 				<h2>Welcome</h2>
+				<?php
+					if(isset($_GET['error'])) { ?>
+					<p class = "error">
+						<?php
+							echo $_GET['error']; ?>
+				<?php } ?>
            		<div class="input-div one ">
            		   <div class="i">
            		   		<i class="fas fa-user"></i>
            		   </div>
            		   <div class="div">
            		   		<h5>Username</h5>
-           		   		<input class="input" type="text">
+           		   		<input class="input" type="text" name = "username">
            		   </div>
            		</div>
            		<div class="input-div two ">
@@ -33,11 +40,11 @@
            		   </div>
            		   <div class="div">
            		    	<h5>Password</h5>
-           		    	<input class="input" type="password">
+           		    	<input class="input" type="password" name = "password">
             	   </div>
             	</div>
             	<a href="#">Forgot Password?</a>
-            	<input type="submit" class="btn" value="Login">
+            	<input type="submit" class="btn" value="Login" name = "login-submit">
             </form>
         </div>
     </div>
